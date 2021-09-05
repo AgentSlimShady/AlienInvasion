afbe5bcb8ebc15d03e7aee42decd1cad0624b98c
AgentSlimShady <pocketid007@gmail.com>
2021-09-06 02:37:21 +0300

    Refactoring: added fn _check_events(self) and _update_screen(self)

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,8 +2,9 @@ import sys
 
 import pygame
 
+from ship import Ship
+from settings import Settings
 
-from settings import  Settings
 
 class AlienInvasion:
     """Загальний клас, що керує песурсами та поведінкою гри"""
@@ -12,22 +13,30 @@ class AlienInvasion:
         """Ініціалізувати гру, створити ресурси гри"""
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion v.0.1a")
+        self.ship = Ship(self)
         # задати колір фону
         self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """"Розпочати головний цикл гри"""
         while True:
-            # слідкувати за подіями клавіатури та миші
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            # наново намалювати екран на кожній ітерації циклу
-            self.screen.fill(self.settings.bg_color)
-            # показати останній намальований екран
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
+        """Реагувати на натискання клавіш та поодії миші"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Оновити зображення на екрані та перемикнутися на новий екран"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # показати останній намальований екран
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ class Settings:
 
     def __init__(self):
         """Ініціалізувати налаштування гри"""
-        #Screen settings
-        self.screen_width=1200
-        self.screen_height=800
-        self.bg_color=(230,230,230)
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
